0abd8b3f656e575dc2f68d3fd286b1650db1bf12
DonaldIvan <developer.donaldivan@gmail.com>
2023-06-09 21:28:22 +0800

    config prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <div>
+      <h1 className="text-green-600">Hello world!</h1>
+      <h1 className="text-yellow-600">Hello world!</h1>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
