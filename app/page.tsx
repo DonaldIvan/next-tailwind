54d6375bd633101c6d4ed5c5f55c594ed61c9f02
DonaldIvan <developer.donaldivan@gmail.com>
2023-06-09 21:34:50 +0800

    custom color

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 const Page = () => {
   return (
     <div>
-      <h1 className="text-green-600">Hello world!</h1>
-      <h1 className="text-yellow-600">Hello world!</h1>
+      <h1 className="border-4 border-green-100">Hello world!</h1>
+      <h1 className="text-vuejs">Hello world!</h1>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        vuejs: '#03fc0f',
+      },
+    },
   },
   plugins: [],
-}
+};
