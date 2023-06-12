3409c918b582cf5ac781f0c8ffbf73fa08e2f06f
DonaldIvan <developer.donaldivan@gmail.com>
2023-06-12 11:44:57 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 const Page = () => {
   return (
     <div>
-      <h1 className="border-4 border-green-100">Hello world!</h1>
-      <h1 className="text-vuejs">Hello world!</h1>
+      <p className="text-xs lg:text-3xl">Hello world!</p>
     </div>
   );
 };
